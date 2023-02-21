45302bb7e56732ae8680dd6fa857405cb686fa66
root <gloria.kitili@student.moringaschool.com>
2023-02-21 20:27:45 +0300

    nitialized with a name as a string and a category as a string The name and category of the magazine can be changed after being initialized.

---
+++ Article.rb
@@ -0,0 +1,16 @@
+class Article
+    attr_reader :title :author :magazine
+
+    @@all = [] 
+
+    def initialize(author,magazine,title)
+        @title = title
+        @author = author
+        @magazine = magazine
+        @@all << self
+    end
+
+    def self.all
+        @@all.dup.freeze
+    end
+end
---
+++ Magazine.rb
@@ -0,0 +1,44 @@
+class Magazine 
+    attr_reader :name :category
+
+    @@all = []
+
+    def initialize(name, category)
+        @name = name 
+        @category = category
+        @contributors = []
+        @@all << self 
+    end 
+
+    def self.all
+        @@all.dup.freeze
+    end
+
+    def self.find_by_name(name)
+        @@all.find{ |magazine| magazine.name == name }
+    end 
+
+    def name=(new_name)
+        @name = new_name
+    end
+
+    def category=(new_category)
+        @category = new_category
+    end
+
+    def contributors
+        @contributors.dup.freeze
+    end 
+
+    def article_titles
+        @contributors << author 
+    end 
+
+    def article_titles 
+        @contributors.flat_map {|author| author.articles.select { |article| article.magazine == self} } 
+    end
+
+    def contributing_authors
+        @contributors.select { |author| author.articles.count { |article| article.magazine == self } >2 }
+    end
+end
